7e313a91067198458a3ca5240180b292a192eb15
Sah2Sah2 <sara.battistella@iths.se>
2025-06-18 15:48:25 +0200

    Play around with design options

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -4,12 +4,16 @@ import "./HomePage.css";
 export default function HomePage({ onSelect }) {
   return (
     <div className="homepage-container">
-      <h2>Keep your favorite running shoes organized, monitor their performance, 
+      <h2>Keep your favorite running shoes organized, 
+        <br/> monitor their performance, 
         <br/> and find the perfect pair for every run.</h2>
       <div className="homepage-buttons">
         <button onClick={() => onSelect("view")}>View Shoes</button>
         <button onClick={() => onSelect("add")}>Add Shoes</button>
       </div>
+
+      <p className="tagline">✨ Ready for your next run? Let's find your perfect match! ✨ </p>
+
     </div>
   );
 }
